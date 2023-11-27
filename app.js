36a432bc32260e7cd9c4888f54157f9035ae7b2e
yh-project <yoha6865@gmail.com>
2023-11-27 22:43:29 +0900

    Feat/#5: 라우터 추가
    post, schedule 라우터 추가

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
 const user_router = require("./router/user");
+const post_router = require("./router/post");
+const schedule_router = require("./router/schedule");
 require("dotenv").config();
 
 app.use(cors()); // cors 미들웨어
@@ -10,6 +12,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/user", user_router);
+app.use("/posts", post_router);
+app.use("/schedules", schedule_router);
 
 app.listen(process.env.EXPRESS_PORT, () => {
   console.log("server is running");
